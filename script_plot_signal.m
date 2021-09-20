0b7adbec842e4a702bfcedf4413ebdf40e225c5d
andreatitti97 <andrea.tiranti97@gmail.com>
2021-09-20 18:32:35 +0200

    FINAL COMMIT

--- script_plot_signal.m
+++ script_plot_signal.m
@@ -1,17 +1,26 @@
 %% Plot signal ARP assignemnt
 clc; close all;
-load('results/signal_10.txt');
-load('results/signal_2.txt');
-sig1_length = length(signal_10);
-sig2_length = length(signal_2);
+%% PUT HERE THE 2 FREQUENCIES USED DURING THE EXPERIMENT
+f1 = "10";
+f2 = "50";
+%% Plot the results
+str = 'results/signal_';
+file1 = strcat(str,f1,'.txt');
+file2 = strcat(str,f2,'.txt');
+signal1 = load(file1);
+signal2 = load(file2);
+sig1_length = length(signal1);
+sig2_length = length(signal2);
 if (sig1_length < sig2_length) 
-    len = length(signal_10);
+    len = length(signal1);
 else
-    len = length(signal_2);
+    len = length(signal2);
 end
 t = [1:len];
 
-plot(t,signal_10(1:len),'k','LineWidth',2);
+plot(t,signal1(1:len),'k','LineWidth',2);
 hold on
-plot(t,signal_2(1:len),'r','LineWidth',2);
-
+plot(t,signal2(1:len),'r','LineWidth',2);
+legend(strcat(f1,'Hz'),strcat(f2,'Hz'));
+xlabel('Time (s)')
+ylabel('Amplitdue')
